ad9abd0ee41bf7635d000ceefb2978f446f9ccbc
Carlos Silva <carlosemilio.contato@gmail.com>
2024-08-20 13:09:20 -0300

    [api] lib: config prisma orm

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/teste', (req, res) => {
+  return 'Hello world'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server runing')
+})
