2b9f0b21c68448b4f424e1715ccc93943010244b
Carlos Silva <carlosemilio.contato@gmail.com>
2024-08-22 15:30:16 -0300

    [api] feat: get participants

---
+++ api/src/routes/get-participants.ts
@@ -0,0 +1,41 @@
+import type { FastifyInstance } from 'fastify'
+import z from 'zod'
+
+import { prisma } from '../lib/prisma'
+
+export async function getParticipants(app: FastifyInstance) {
+  app.get('/trips/:tripId/participants', async (request, reply) => {
+    const schema = z.object({
+      params: z.object({
+        tripId: z.string().uuid()
+      }),
+    })
+
+    const parsed = schema.safeParse(request)
+
+    if (!parsed.success) {
+      return reply.status(400).send({ error: parsed.error.errors })
+    }
+
+    const { tripId } = request.params as any
+
+    const trip = await prisma.trip.findUnique({
+      where: { id: tripId },
+      include: {
+        participants: {
+          select: { id: true, name: true, email: true, is_confirmed: true }
+        }
+      }
+    })
+
+    if (!trip) {
+      return reply.status(404).send({
+        error: 'Trip not found.'
+      })
+    }
+
+    return {
+      participants: trip.participants
+    }
+  })
+}
