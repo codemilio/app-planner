bcf3a17f2c22cb0e87421e72557e60881d369eec
Carlos Silva <carlosemilio.contato@gmail.com>
2024-08-22 11:36:35 -0300

    [api] feat: confirm-participant route

---
+++ api/src/routes/confirm-participant.ts
@@ -0,0 +1,41 @@
+import type { FastifyInstance } from 'fastify'
+import z from 'zod'
+
+import { prisma } from '../lib/prisma'
+
+
+export async function confirmParticipant(app: FastifyInstance) {
+  app.get('/participants/:participantId/confirm', async (request, reply) => {
+    const schema = z.object({
+      participantId: z.string().uuid()
+    })
+
+    const parsed = schema.safeParse(request.params)
+
+    if (!parsed.success) {
+      return reply.status(400).send({ error: parsed.error.errors })
+    }
+
+    const { participantId } = request.params as any
+    const participant = await prisma.participant.findUnique({
+      where: {
+        id: participantId
+      },
+    })
+
+    if (!participant) {
+      return reply.status(404).send({ error: 'Participant not found.' })
+    }
+
+    if (participant.is_confirmed) {
+      return reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`)
+    }
+
+    await prisma.participant.update({
+      where: { id: participantId },
+      data: { is_confirmed: true }
+    })
+   
+    return reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`)
+  })
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify'
 
 import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipant } from './routes/confirm-participant'
 
 const app = fastify()
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server runing')
