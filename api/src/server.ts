284018c8fe9ff6127e3897c06acc96ad41b8a40a
Carlos Silva <carlosemilio.contato@gmail.com>
2024-08-22 15:50:16 -0300

    [api] fix: register new routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -7,16 +7,31 @@ import { createActivity } from './routes/create-activity'
 import { createLink } from './routes/create-link'
 import { getActivities } from './routes/get-activities'
 import { getLink } from './routes/get-links'
+import { getParticipants } from './routes/get-participants'
+import { createInvite } from './routes/create-invite'
+import { updateTrip } from './routes/update-trip'
 
 const app = fastify()
 
+// Trip
 app.register(confirmTrip)
-app.register(confirmParticipant)
 app.register(createTrip)
-app.register(createActivity)
+app.register(updateTrip)
+
+// Participants
+app.register(confirmParticipant)
+app.register(getParticipants)
+
+// Link 
 app.register(createLink)
-app.register(getActivities)
 app.register(getLink)
+
+// Activities 
+app.register(createActivity)
+app.register(getActivities)
+
+// Invite
+app.register(createInvite)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server runing')
