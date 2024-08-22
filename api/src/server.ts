1fd2367065c85ecb32cf380d597a231769621495
Carlos Silva <carlosemilio.contato@gmail.com>
2024-08-21 22:21:35 -0300

    [api] chore: register create-trip route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,24 +1,11 @@
 import fastify from 'fastify'
 import { prisma } from './lib/prisma'
 
+import { createTrip } from './routes/create-trip'
+
 const app = fastify()
 
-app.get('/register', async (req, res) => {
-  prisma.trip.create({
-    data: {
-      destination: 'floripa',
-      ends_at: new Date(),
-      starts_at: new Date(),
-    }
-  })
-
-  return 'success register'
-})
-
-app.get('/list', async (req, res) => {
-  const trips = await prisma.trip.findMany()
-  return trips
-})
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server runing')
